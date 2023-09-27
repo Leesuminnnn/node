843049a78b210c48da2a10eb846c511e752a44e6
leesuminnnn <fj1093@naver.com>
2023-09-27 11:39:29 +0900

    fs모듈을 통한 로그인 기능 구현

--- server/src/models/UserStorage.js
+++ server/src/models/UserStorage.js
@@ -1,14 +1,24 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage{
-    static #users = {
-        id: ["test", "test1", "test2"],
-        password: ["1234", "12345", "123456"],
-        name: ["홍길동", "관리자", "회원"]
-    };
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data)
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   // => [id, password, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
+    
 
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -17,20 +27,21 @@ class UserStorage{
         }, {});
         return newUsers;
     }
-    static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);   // => [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
 
-        return userInfo;
+    static getUserInfo(id) {
+        return fs.readFile("./server/src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+            
+        })
+        .catch(console.error);
+
     }
 
+    
+
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.password.push(userInfo.password);
